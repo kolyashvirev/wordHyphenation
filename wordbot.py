2f6f6ef82aa29b1504c4fcb7078d23135af404ce
kolyashvirev <68767709+kolyashvirev@users.noreply.github.com>
2025-01-14 01:07:19 +0700

    Update wordbot.py

--- wordbot.py
+++ wordbot.py
@@ -22,7 +22,7 @@ logging.basicConfig (
     format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level = logging.INFO
 )
-ogging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 # Настройка Selenium WebDriver
